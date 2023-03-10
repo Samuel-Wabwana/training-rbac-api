e047699e7adea75d904bc1fb90b97077f3cee325
Samuel Wabwana <samuelwabwana@gmail>
2023-03-10 07:30:25 -0800

    Configuration de mikroorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    UsersModule,
+    AuthModule,
+    ConfigModule.forRoot({
+    cache: true,
+  }),
+  MikroOrmModule.forRoot({
+    entities: ['./dist/entities'],
+    entitiesTs: ['./src/entities'],
+    dbName: process.env.DB_NAME,
+    type: 'postgresql',
+    password: process.env.DB_PASS,
+  }),
+],
   controllers: [AppController],
   providers: [AppService],
 })
